3775fd9c2e9dcabe915bbc3803aff129af5aafc0
BMapetitverd <apetitverd@cifpfbmoll.eu>
2025-03-30 13:05:39 +0200

    first exercice

---
+++ 00.Select/main.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+# Conexión a MySQL (ajusta user, password y host según tu configuración)
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="admin",      # Usuario de MySQL
+    password="Curs202425",      # Contraseña (vacía por defecto en XAMPP/WAMP)
+    database="classicmodels"
+)
+
+cursor = conexion.cursor()
+
+# Select and show all the customers
+cursor.execute("SELECT * FROM Customers")
+print("All the customers:")
+for customer in cursor.fetchall():
+    print(customer)
+
+# Select and show all the customers with a creditLimit greater than 100.000
+cursor.execute("SELECT * FROM Customers WHERE creditLimit > 100000")
+print("\nCustomers with more than 100000:")
+for customer in cursor.fetchall():
+    print(customer)
+
+conexion.close()
