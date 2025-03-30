f80b3ca3589f3d264f882b202bcfbb9f444ca0c9
BMapetitverd <apetitverd@cifpfbmoll.eu>
2025-03-30 13:35:55 +0200

    changes on first exercice

--- 00.Select/main.py
+++ 00.Select/main.py
@@ -1,3 +1,4 @@
+#Use pip install mysql-connector-python if you don't have this package installed
 import mysql.connector
 
 # Conexión a MySQL (ajusta user, password y host según tu configuración)
@@ -17,9 +18,10 @@ for customer in cursor.fetchall():
     print(customer)
 
 # Select and show all the customers with a creditLimit greater than 100.000
-cursor.execute("SELECT * FROM Customers WHERE creditLimit > 100000")
+cursor.execute("SELECT customerNumber, customerName, creditLimit FROM Customers WHERE creditLimit > 100000")
 print("\nCustomers with more than 100000:")
 for customer in cursor.fetchall():
     print(customer)
+    print(customer[1])
 
 conexion.close()
