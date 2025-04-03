40a594b8c4554472ffe37d2f068c1925d544e68a
BMapetitverd <apetitverd@cifpfbmoll.eu>
2025-04-03 20:31:20 +0200

    Ejercicio con bonus

---
+++ nuke.py
@@ -0,0 +1,34 @@
+import mysql.connector
+import random
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="admin",      # MySQL user
+    password="Curs202425",      # user's password
+    database="classicmodels"
+)
+
+connectionBM = mysql.connector.connect(
+    host="localhost",
+    user="admin",      # MySQL user
+    password="Curs202425",      # user's password
+    database="borjaMoll"
+)
+
+cursor = connection.cursor()
+cursorBM = connectionBM.cursor()
+
+cursorBM.execute("USE borjaMoll")
+
+cursor.execute("SELECT customerNumber, contactFirstName, contactLastName FROM Customers")
+
+"""
+for customer in cursor.fetchall():
+    cursorBM.execute("INSERT INTO Students (Id, name, lastName) Values(%s,%s,%s)",(customer[0],customer[1],customer[2]))
+"""
+studentsToInsert = []
+for customer in cursor.fetchall():
+    studentsToInsert.append((customer[0], customer[1], customer[2], random.randrange(18, 41)))
+
+cursorBM.executemany("INSERT INTO Students (Id, name, lastName) Values(%s,%s,%s)",studentsToInsert)
+#connectionBM.commit()
